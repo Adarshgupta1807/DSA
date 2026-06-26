ddb6063d8663d9f32b71ea8f6868bae3a24995e1
Adarshgupta1807 <gupta.adarsh1872005@gmail.com>
2026-06-26 12:53:37 +0530

    Time: 0 ms (100%), Space: 27.4 MB (6.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,19 +4,19 @@ public:
         int n=height.size();
         vector<int> left(n);
         int maxl=height[0];
-        for(int i=0;i<n;i++){
-            maxl=max(height[i],maxl);
+        for(int i=0;i<height.size();i++){
+            maxl=max(maxl,height[i]);
             left[i]=maxl;
         }
         vector<int> right(n);
         int maxr=height[n-1];
         for(int i=n-1;i>=0;i--){
-            maxr=max(height[i],maxr);
+            maxr=max(maxr,height[i]);
             right[i]=maxr;
         }
         int sum=0;
         for(int i=0;i<n;i++){
-            int temp=min(left[i],right[i]);
+            int temp=min(right[i],left[i]);
             int t=temp-height[i];
             sum=sum+t;
         }
