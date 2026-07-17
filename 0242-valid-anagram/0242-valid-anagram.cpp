0eeaae7e094016fdca02a0ac92581852abfceaed
Adarsh Gupta <gupta.adarsh1872005@gmail.com>
2026-07-17 11:45:30 +0530

    Time: 3 ms (45.97%), Space: 9.8 MB (49.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()>t.length()) return false;
-        unordered_map<char,int> count;
-        for(char ch:s){
-            count[ch]++;
-        }
-        for(char cht:t){
-            if(count.find(cht)==count.end() || count[cht]==0){
-                return false;
-            }
-            else{
-                count[cht]--;
-            }
-        }
-        return true;
+        if(s.length()!=t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
