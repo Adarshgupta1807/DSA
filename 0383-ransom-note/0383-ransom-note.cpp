d764472157795d4c9a64bd834854987f103dc330
Adarsh Gupta <gupta.adarsh1872005@gmail.com>
2026-08-18 10:38:49 +0530

    Time: 0 ms (100%), Space: 12.2 MB (12.19%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> rpp;
         unordered_map<char,int> mpp;
+        for(char ch:ransomNote){
+            rpp[ch]++;
+        }
         for(char ch:magazine){
             mpp[ch]++;
         }
-        for(char ch:ransomNote){
-            if(mpp[ch]<=0){
-                return false;
-            }
-            else{
-                mpp[ch]--;
-            }
+        for(char ch='a';ch<='z';ch++){
+            if(mpp[ch]<rpp[ch]) return false;
         }
         return true;
     }
